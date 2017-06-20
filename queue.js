a4095e8f47830f82f57e656e90fd2fb9871c2bcf
ccjjyy <153336897@qq.com>
2017-06-20 17:33:40 +0800

    Update queue.js

--- queue.js
+++ queue.js
@@ -12,6 +12,9 @@ function Queue(){
     this.toString = toString;
     this.empty = empty;
     this.count = count;
+
+    this.p_dequeue = p_dequeue;
+    this.p_toString = p_toString;
 }
 
 function enqueue(element){
@@ -19,7 +22,7 @@ function enqueue(element){
 }
 
 function dequeue(){
-    return this.dataStore.shift();
+    return this.dataStore.shift(); // element
 }
 
 function front(){
@@ -49,6 +52,26 @@ function empty(){
 
 function count(){
     return this.dataStore.length;
+}
+
+function p_dequeue(){
+    var priority = this.dataStore[0].code;
+    var pos = 0;
+    for(var i=1; i<this.dataStore.length; i++){
+        if(this.dataStore[i].code < priority){
+            priority = this.dataStore[i].code;
+            pos = i;
+        }
+    }
+    return this.dataStore.splice(pos, 1); // array
+}
+
+function p_toString(){
+    var str = "";
+    for(var i=0; i<this.dataStore.length; i++){
+        str += this.dataStore[i].name + " code: " + this.dataStore[i].code + "\n";
+    }
+    return str;
 }
 
 // test
@@ -123,3 +146,79 @@ if(males.count() > 0){
 if(females.count() > 0){
     console.log("There are still " + females.count() + " female dancers waiting to dance.");
 }
+
+
+// Radix Sorting
+function distribute(nums, queues, digit){
+    for(var i=0; i<nums.length; i++){
+        if(digit == 1){
+            queues[nums[i] % 10].enqueue(nums[i]);
+        }
+        else if(digit == 10){
+            queues[Math.floor(nums[i] / 10)].enqueue(nums[i]);
+        }
+        else{
+            console.log("ERROR: Digit must be 1 or 10.");
+        }
+    }
+}
+
+function collect(queues, nums){
+    var i = 0;
+    for(var j=0; j<10; j++){
+        while(!queues[j].empty()){
+            nums[i++] = queues[j].dequeue();
+        }
+    }
+}
+
+function dispArray(arr){
+    for(var i=0; i<arr.length; i++){
+        console.log(arr[i] + " ");
+    }
+}
+
+var length = 8;
+var queues = [];
+for(var i=0; i<10; i++){
+    queues[i] = new Queue();
+}
+var nums = [];
+for(var i=0; i<length; i++){
+    nums[i] = Math.floor(Math.random() * 99);
+}
+
+console.log("Before radix sort: ");
+dispArray(nums);
+console.log("Sorted by 1: ");
+distribute(nums, queues, 1);
+collect(queues, nums);
+dispArray(nums);
+console.log("Sorted by 10: ");
+distribute(nums, queues, 10);
+collect(queues, nums);
+dispArray(nums);
+
+
+// priority queue
+function Patient(name, code){
+    this.name = name;
+    this.code = code;
+}
+
+
+var q = new Queue();
+q.enqueue(new Patient("Smith", 5));
+q.enqueue(new Patient("Jones", 4));
+q.enqueue(new Patient("Fehrenbach", 6));
+q.enqueue(new Patient("Brown", 1));
+q.enqueue(new Patient("Ingram", 1));
+console.log(q.p_toString());
+
+console.log("Patient being treated: " + q.p_dequeue()[0].name);
+console.log("Patient waiting to be seen: ");
+console.log(q.p_toString());
+
+console.log("Patient being treated: " + q.p_dequeue()[0].name);
+console.log("Patient waiting to be seen: ");
+console.log(q.p_toString());
